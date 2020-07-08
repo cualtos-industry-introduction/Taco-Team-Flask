f5e34618366c830e8c336db284cc17e3424444c9
fernandocornejo <fernando.cornejog@academicos.udg.mx>
2020-07-08 18:26:07 -0500

    Update

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Obdulia\TacoTeam-Flask
+set VIRTUAL_ENV=C:\corne\Desktop\Agenda-Flask
 
 if not defined PROMPT set PROMPT=$P$G
 
