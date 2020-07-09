6812e03b5d8eb9eb50f30fd904b54e618b858a7d
fernandocornejo <fernando.cornejog@academicos.udg.mx>
2020-07-09 00:10:52 -0500

    Actualización de vistas web, opción de mostrar todo completa, actualización de módulo en app.py

--- app.py
+++ app.py
@@ -1,32 +1,34 @@
 from flask import Flask, render_template, request
+from agenda import Agenda
+from contacto import Contacto
+from jinja2 import Template
 import json
 
 app = Flask(__name__)
 
 #index inicio
-@app.route('/index')
+@app.route('/')
 def index():
-    
-    return render_template('index.html')
+    #return render_template('index.html')
+    agenda = Agenda('agenda')
+    contactos = agenda.obtenerContactos()
+    return render_template('index.html', contactos=contactos)
 
 #agregar
 @app.route('/agregar')
 def agregar():
-    
     return render_template('agregar.html')
 
 #buscar
 @app.route('/buscar')
 def buscar():
-    
     return render_template('buscar.html')
 
- 
-@app.route('/agenda/mostrar')
-def mostrar_agenda():
-    agenda = Agenda('agenda')
-    contactos = agenda.obtenerContactos()
-    return render_template('mostrar_agenda.html', contactos=contactos)
+#@app.route('/agenda/mostrar')
+#def mostrar_agenda():
+#    agenda = Agenda('agenda')
+#    contactos = agenda.obtenerContactos()
+#    return render_template('mostrar_agenda.html', contactos=contactos)
     
 if __name__ == "__main__":
     app.run()
