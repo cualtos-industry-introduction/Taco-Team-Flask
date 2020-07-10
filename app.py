0a0050568f1545cb19453d1fea907c5393b9697a
fernandocornejo <fernando.cornejog@academicos.udg.mx>
2020-07-09 19:27:30 -0500

    Completo módulo insertar

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from agenda import Agenda
 from contacto import Contacto
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from jinja2 import Template
 import json
 from consultas import obtenerTodo, obtenerUno, insertarUno
@@ -13,14 +13,32 @@ def index():
     return render_template('index.html')
 
 #agregar
-@app.route('/agregar')
+@app.route('/agregar', methods=['GET', 'POST'])
 def agregar():
-    return render_template('agregar.html')
+    if request.method == 'POST':
+        datos = {}
+        datos['nombre'] = request.form['nombre']
+        datos['empresa'] = request.form['empresa']
+        datos['correo'] = request.form['correo']
+        datos['telefono'] = request.form['telefono']
+        datos['nota'] = request.form['nota']
+        insertarUno(datos)
+        return redirect(url_for('mostrar_agenda'))
+    elif request.method == 'GET':
+        return render_template('agregar.html')
 
 #buscar
-@app.route('/buscar')
+@app.route('/buscar', methods=['GET', 'POST'])
 def buscar():
-    return render_template('buscar.html')
+    if request.method == 'POST':
+        return redirect(url_for('buscar_resultado', nombre=request.form['nombre']))
+    elif request.method == 'GET':
+        return render_template('buscar.html')
+
+@app.route('/buscar/<nombre>')
+def buscar_resultado(nombre):
+    resultado = obtenerUno(nombre)
+    return render_template('buscarres.html', resultado=resultado)
  
 @app.route('/mostrar')
 def mostrar_agenda():
